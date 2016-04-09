70ea88ff1bc238b8ddaa8b04f353437f96e5141d
Drew Wyatt <drew.wyatt@bottlerocketstudios.com>
2016-04-09 16:36:29 -0500

    added better sample inputs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import {MagicSquare} from './magicsquare';
 
-const ms = new MagicSquare([1,2,3,1,2,3,1,2,3]);
-console.log(ms.isValid);
+console.log(new MagicSquare([8, 1, 6, 3, 5, 7, 4, 9, 2]).isValid);
+console.log(new MagicSquare([2, 7, 6, 9, 5, 1, 4, 3, 8]).isValid);
+console.log(new MagicSquare([3, 5, 7, 8, 1, 6, 4, 9, 2]).isValid);
+console.log(new MagicSquare([8, 1, 6, 7, 5, 3, 4, 9, 2]).isValid);
